15ba2ce7a13650bf4d8ee21f4f6fd6bf7f09633d
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2021-02-02 12:55:51 +0100

    Database fixed (working!!)

---
+++ src/databaseConfig.java
@@ -0,0 +1,7 @@
+public class databaseConfig {
+    static String user = "rasmus";
+    static String password = "rasmus360";
+    static String port = "3306";
+    static String DBname = "te18";
+    static String DBURL = "localhost";
+}
